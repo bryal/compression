ef403869a03cbbd881126df629bb796aaa9fd502
Johan Johansson <96.bryal@gmail.com>
2017-09-15 17:23:50 +0200

    Less repetition

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ import sys
 
 def compress(filename):
     sample_rate, samples = scipy.io.wavfile.read(filename)
-    left, right = samples.T
-    left_ft, right_ft = rfft(left), rfft(right)
-    original_ft_len = len(left_ft)
-    left_ft_high_cut = numpy.complex64(left_ft[0:original_ft_len//4])
-    right_ft_high_cut = numpy.complex64(right_ft[0:original_ft_len//4])
+    channels = samples.T
+    ft = [x for x in map(rfft, channels)]
+    original_ft_len = len(ft[0])
+    high_cut = map(lambda l: numpy.complex64(l[0:len(l)//4]), ft)
 
     out_filename = filename + ".compressed"
     with open(out_filename, "wb") as out:
         out.write(sample_rate.to_bytes(4, byteorder="big"))
         out.write(original_ft_len.to_bytes(4, byteorder="big"))
-        out.write(left_ft_high_cut.tobytes())
-        out.write(right_ft_high_cut.tobytes())
+        for compressed in high_cut:
+            out.write(compressed.tobytes())
 
 def uncompress(filename):
     with open(filename, "rb") as f:
@@ -26,15 +25,13 @@ def uncompress(filename):
         original_ft_len = int.from_bytes(f.read(4), "big")
         shortened_len = original_ft_len // 4
         channel_n_bytes = shortened_len * 8
-        left_bytes = f.read(channel_n_bytes)
-        right_bytes = f.read(channel_n_bytes)
-        left_ft_high_cut = numpy.fromstring(left_bytes, dtype=numpy.complex64)
-        right_ft_high_cut = numpy.fromstring(right_bytes, dtype=numpy.complex64)
         pad_width = original_ft_len - shortened_len
-        left_ft_padded = numpy.pad(left_ft_high_cut, (0, pad_width), "constant")
-        right_ft_padded = numpy.pad(right_ft_high_cut, (0, pad_width), "constant")
-        left, right = irfft(left_ft_padded), irfft(right_ft_padded)
-        uncompressed_samples = numpy.array(numpy.around([left, right]), dtype=numpy.int16).T
+        compr_bytes = [f.read(channel_n_bytes), f.read(channel_n_bytes)]
+        high_cut = map(lambda bs: numpy.fromstring(bs, dtype=numpy.complex64), compr_bytes)
+        padded = map(lambda l: numpy.pad(l, (0, pad_width), "constant"), high_cut)
+        channels = map(irfft, padded)
+        rounded = numpy.around([x for x in channels])
+        uncompressed_samples = numpy.array(rounded, dtype=numpy.int16).T
 
         out_filename = filename + ".uncompressed.wav"
         scipy.io.wavfile.write(out_filename, sample_rate, uncompressed_samples)
